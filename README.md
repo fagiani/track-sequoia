748475d3fb9674ebf9207a2c5cfa8ea6efc93b72
Paulo Fagiani <pfagiani@gmail.com>
2021-10-10 12:40:08 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Track Sequoia
+📦 Terminal Track Sequoia
+
+[![Github release](https://img.shields.io/github/v/release/fagiani/track-sequoia?logo=github)](https://github.com/fagiani/track-sequoia/releases/latest)
+[![Package release](https://img.shields.io/github/package-json/v/fagiani/track-sequoia?label=package.json%20version)](https://github.com/fagiani/track-sequoia/releases/latest)
+[![npm release](https://img.shields.io/npm/v/track-sequoia?logo=npm)](https://www.npmjs.com/package/track-sequoia)
+[![Github issues](https://img.shields.io/github/issues/fagiani/track-sequoia)](https://github.com/fagiani/track-sequoia/issues)
+[![Github license](https://img.shields.io/github/license/fagiani/track-sequoia)](https://github.com/fagiani/track-sequoia/blob/master/LICENSE.txt)
+
+## 🚀 Demo
+
+![screenshot](screenshot.png)
+
+## ✨ Usage
+
+```
+npx track-sequoia [code]
+```
+
+## 📣 Contributing
+
+If you want to contribute please fork the repository and get your hands dirty, and make the changes as you'd like and submit the Pull Request. Any questions, open a issue.
+
+## 👏 Aknowlegement
+
+This project was inspired by [Maurício Mutte](https://github.com/mauriciomutte)'s project [Terminal Track Correios](https://github.com/mauriciomutte/track-correios).
---
+++ src/index.js
@@ -0,0 +1,158 @@
+import fetch from 'node-fetch';
+import chalk from 'chalk';
+
+const { log } = console;
+const logEnter = (text) => {
+	log(text);
+	log();
+};
+
+const iconByStatus = {
+	'A Caminho': '🚚',
+	'Em transferência': '🚚',
+	'Envio em rua': '🙌',
+	'Em processo de entrega': '🙌',
+	'Entregue': '🎁',
+	'Entrega realizada normalmente': '🎁',
+	'Endereço insuficiente': '⚠️',
+	'Destinatário ausente': '⚠️',
+	'Ausente': '⚠️',
+	'Pedido Recebido': '🛬',
+	'Envio encaminhado': '🛬',
+	'Recepção na transportadora': '📦',
+	'Recepcionado': '📦',
+	DEFAULT: '🚧',
+};
+
+function getIcon(status) {
+	return iconByStatus[status] || iconByStatus.DEFAULT;
+}
+
+async function getHash() {
+	const url = 'https://www.sequoialog.com.br/en/rastreio/';
+	const options = { redirect: 'manual' }
+
+	const response = await fetch(url, options);
+
+	if ( response.status != '301' ) {
+		log("❌ Retorno inesperado:", response.status);
+		return null;
+	}
+
+	const hash = await response.headers.get('location').split('/').pop();
+
+	return hash;
+}
+
+async function getCookie(hash) {
+	const url = 'https://portalvesta-api.sequoialog.com.br/.auth/cookieExternal';
+	const options = { method: 'POST', body: JSON.stringify({ hash }), headers: { 'Content-Type': 'application/json'}};
+
+	const response = await fetch(url, options);
+
+	if ( ! response.ok ) {
+		log("❌ Retorno inesperado:", response.status);
+		return null;
+	}
+
+	const raw = await response.headers.raw()['set-cookie'];
+
+	return raw.map((entry) => entry.split(';')[0]).join(';');
+}
+
+async function getOrders(code, cookie) {
+	let orders = await getOrdersByTracker(code, cookie);
+	if ( orders.length === 0 ) {
+		orders = await getOrdersByCpf(code, cookie)
+	}
+
+	return orders;
+}
+
+async function getOrdersByTracker(code, cookie) {
+	const url = `https://portalvesta-api.sequoialog.com.br/ConsultaDetalhes/SelecionarRastreamentoPedidos?codRastreioNumPedido=${code}`;
+	const options = { headers: { cookie } };
+
+	const response = await fetch(url, options);
+	
+	if ( ! response.ok ) {
+		log("❌ Retorno inesperado:", response.status);
+		return null;
+	}
+
+	const orders = await response.json();
+	
+	return orders;
+}
+
+async function getOrdersByCpf(code, cookie) {
+	const url = `https://portalvesta-api.sequoialog.com.br/ConsultaDetalhes/SelecionarRastreamentoPedidos?cpf=${code}&itensPorPagina=2500`;
+	const options = { headers: { cookie } };
+
+	const response = await fetch(url, options);
+	
+	if ( ! response.ok ) {
+		log("❌ Retorno inesperado:", response.status);
+		return null;
+	}
+
+	const orders = await response.json();
+	
+	return orders;
+}
+
+async function getEvents(order, cookie) {
+	const url = `https://portalvesta-api.sequoialog.com.br/ConsultaDetalhes/SelecionarRastreamentoPedidoHistorico/${order}`;
+	const options = { headers: { cookie } };
+
+	const response = await fetch(url, options);
+	
+	if ( ! response.ok ) {
+		log("❌ Retorno inesperado:", response.status);
+		return null;
+	}
+
+	const events = await response.json();
+	
+	return events;
+}
+
+export default async function run() {
+	const code = process?.argv[2]?.toUpperCase();
+
+	if (!code) {
+		log(`🖊️Informe o código de rastreio ou CPF/CNPJ para que a consulta seja realizada!`);
+		return null;
+	}
+
+	logEnter(chalk.bold(`📮 ${code}`));
+
+	const hash = await getHash();
+	const cookie = await getCookie(hash);
+	const orders = await getOrders(code, cookie);
+
+	if ( orders.length === 0 ) {
+		logEnter(chalk.bold('Não foram encontrados registros para este código!'))
+	}
+
+	orders?.reverse().forEach(async order => {
+		const events = await getEvents(order.pedidoID, cookie);
+
+		log(`Remessa: ${order.documento}`)
+		log(`Data: ${new Date(order.data).toLocaleDateString()}`)
+		log(`Loja de Compra: ${order.nomeCliente}`)
+		log(`Situação: ${order.situacao}`)
+		log();
+			
+		events?.reverse().forEach((event) => {
+			log(`==> ${getIcon(event.situacao)} ${event.situacao}`);
+			log(chalk.blackBright(`Data e hora: ${new Date(event.data).toLocaleString()}`));
+			if( event.localidade ) {
+				log(chalk.blackBright(`Localidade: ${event.localidade}`));
+			}
+			log();
+		});
+	});
+
+	return null;
+}
